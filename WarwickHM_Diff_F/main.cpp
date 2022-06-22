33a34755a4aac42f2015d46bb8206361eda75066
Warwick <WarwickSeven@gmail.com>
2022-06-22 20:04:02 +0300

    Initial Commit

---
+++ WarwickHM_Diff_F/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WarwickHM_Diff_F
+//
+//  Created by Александр Тихонов on 22.06.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
