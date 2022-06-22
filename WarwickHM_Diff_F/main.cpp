877b26a6b78babc88b2549c67e0cffd0d802b8c8
Warwick <WarwickSeven@gmail.com>
2022-06-23 02:23:02 +0300

    Lesson F Task 2

--- WarwickHM_Diff_F/main.cpp
+++ WarwickHM_Diff_F/main.cpp
@@ -3,47 +3,79 @@
 #include <thread>
 #include <vector>
 
-//глобальные переменные это плохо
-int prog = 0;
-int result = 0;
-
-void findPN(const int &numPN) {
-    std::vector<int> PNs;
-    PNs.reserve(numPN);
-    PNs.push_back(2);
-    int n = 3;
-    bool isPrime;
-    for (int k = 1; k < numPN; k++) {
-        isPrime = true;
-        for (int i = 2; i < n; i++) {
-                if (n % i == 0) {
+class PrimeNumber {
+    int m_last;                         //номер искомого числа
+    int m_cur;                          //текущее простое число
+    int m_result;                       //n-ное простое число
+    int m_prog;                         //текущий прогресс в %
+    std::vector<int> m_vec;             //вектор найденных простых чисел
+    std::vector<char> v_prog;    //вектор шкалы прогресса
+public:
+    PrimeNumber(const int &last) : m_last(last) {
+        v_prog.resize(10);
+        for (int i = 0; i < 10; i++)
+            v_prog[i] = '_';
+    }
+    
+    int calcPM() {
+        m_vec.reserve(m_last);
+        m_vec.push_back(2);
+        m_cur = 3;
+        bool isPrime;
+        for (int k = 1; k < m_last; k++) {
+            isPrime = true;
+            for (int i = 2; i < m_cur; i++) {
+                if (m_cur % i == 0) {
                     isPrime = false;
                     k--;
                     break;
                 }
+            }
+            if (isPrime == true) {
+                m_vec.push_back(m_cur);
+                std::this_thread::sleep_for(std::chrono::milliseconds(10)); //замедлитель вычислений
+                m_prog = static_cast<int>((m_vec.size()*100)/m_vec.capacity());
+            }
+            m_cur++;
         }
-        if (isPrime == true) {
-            PNs.push_back(n);
-            std::this_thread::sleep_for(std::chrono::milliseconds(10)); //замедлитель вычислений
-            prog = static_cast<int>((PNs.size()*100)/PNs.capacity());
-        }
-        n++;
+        m_result = m_vec[m_vec.size()-1];
+        return m_result;
     }
-    result = PNs[PNs.size()-1];
-}
+    int getProg() {
+        return m_prog;
+    }
+    
+    int getResult() {
+        return m_result;
+    }
+    
+    void printProgress() {
+        for (int i = 1; (i*10) <= m_prog; i++ ) {
+            if ((i*10) <= m_prog)
+                v_prog[i-1] = 'X';
+        }
+        for (auto j : v_prog) {
+            std::cout << j;
+        }
+        std::cout << "  " << m_prog << "%" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000)); //замедление отображения
+    }
+};
 
 int main() {
-    std::cout << "Enter the № of the prime number ( > 3 ): ";
-    int numPN;
-    std::cin >> numPN;
-    std::thread th1(findPN, numPN);
-    th1.detach();
-    while (prog < 100) {
-        std::cout << prog << "%" <<std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000)); //замедлитель отображения
+    int numPN = 0;
+    while (numPN <= 1) {
+        std::cout << "Enter the № of the prime number ( > 1 ): ";
+        std::cin >> numPN;
     }
-    if (prog == 100) {
-        std::cout << "Complete! " << numPN <<" Primal number is: " << result << std::endl;
+    PrimeNumber pn1(numPN);
+    std::thread th1(&PrimeNumber::calcPM, &pn1);
+    th1.detach();
+    while (pn1.getProg() < 100) {
+        pn1.printProgress();
+    }
+    if (pn1.getProg() == 100) {
+        std::cout << "Complete! " << numPN << " Primal number is: " << pn1.getResult() << std::endl;
     }
     return 0;
 }
